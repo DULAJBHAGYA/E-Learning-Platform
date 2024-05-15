ee7d5d8817b0203959fedef62a0c718492cd3e6b
unknown <dulajupananda@gmail.com>
2024-05-15 12:02:09 +0530

    developed course description page
    developed course description page with lessons and about course

--- e_learning/lib/student/course display/courseDescription.dart
+++ e_learning/lib/student/course display/courseDescription.dart
@@ -1,9 +1,9 @@
+import 'package:e_learning/student/all%20courses/allCourses.dart';
 import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../../color.dart'; 
-
+import '../../color.dart';
 
 class CourseDescription extends StatefulWidget {
   const CourseDescription({Key? key}) : super(key: key);
@@ -51,11 +51,17 @@ class _CourseDescriptionState extends State<CourseDescription>
                       padding: const EdgeInsets.all(20.0),
                       child: Column(
                         children: [
-                          Icon(
-                            EneftyIcons.arrow_left_4_outline,
-                            size: 30,
-                            color: white,
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => const StdAllCourses()));
+                            },
+                            child: Icon(
+                              EneftyIcons.arrow_left_4_outline,
+                              size: 30,
+                              color: Colors.white,
+                            ),
                           ),
+
                         ],
                       ),
                     ),
@@ -104,7 +110,7 @@ class _CourseDescriptionState extends State<CourseDescription>
                                   style: GoogleFonts.poppins(
                                       fontSize: 15,
                                       fontWeight: FontWeight.w400,
-                                      color: black)),
+                                      color:black)),
                             ],
                           ),
                           SizedBox(width: 15),
@@ -124,7 +130,7 @@ class _CourseDescriptionState extends State<CourseDescription>
                           Row(
                             children: [
                               Icon(EneftyIcons.document_bold,
-                                  size: 20, color:blue),
+                                  size: 20, color: blue),
                               SizedBox(width: 10),
                               Text('Certificate',
                                   style: GoogleFonts.poppins(
@@ -163,7 +169,7 @@ class _CourseDescriptionState extends State<CourseDescription>
                         indicator: BoxDecoration(
                           border: Border(
                             bottom: BorderSide(
-                              color: Colors.blue,
+                              color: blue,
                               width: 2,
                               style: BorderStyle.solid,
                             ),
@@ -171,22 +177,18 @@ class _CourseDescriptionState extends State<CourseDescription>
                         ),
                       ),
                       SizedBox(height: 20),
-                      SingleChildScrollView(
-                        child: Container(
-                          height: MediaQuery.of(context).size.height * 0.5,
-                          child: TabBarView(
-                            controller: _tabController,
-                            children: [
-                              // Content of the first tab ('About')
-                              AboutCourse(),
-                              // Content of the second tab ('Lessons')
-                              Text('Lessons',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black)),
-                            ],
-                          ),
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        child: TabBarView(
+                          controller: _tabController,
+                          children: [
+                            // Content of the first tab ('About')
+                            SingleChildScrollView(child: AboutCourse()),
+                            // Content of the second tab ('Lessons')
+                            SingleChildScrollView(
+                              child: Lessons(),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -201,10 +203,86 @@ class _CourseDescriptionState extends State<CourseDescription>
   }
 }
 
-class AboutCourse extends StatelessWidget {
-  const AboutCourse({
+class Lessons extends StatelessWidget {
+  const Lessons({
     super.key,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          ListView(
+            shrinkWrap: true,
+            children: [
+              ListTile(
+                leading: Icon(EneftyIcons.chart_success_outline, color: black),
+                title: Text('Introduction to Data Science', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500, color: black),),
+                subtitle: Text('Students will learn about the role of data science in today\'s world, its applications across various industries, and the skills required to become proficient in the field.', 
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400, color: black),),
+              ),
+              SizedBox(height: 10),
+
+
+              ListTile(
+                leading: Icon(EneftyIcons.chart_success_outline, color: black),
+                title: Text('Data Exploration and Visualization', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500, color: black),),
+                subtitle: Text('This module will cover techniques for exploring and visualizing data, including descriptive statistics, data cleaning, and visualization libraries such as Matplotlib and Seaborn.', 
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400, color: black),),
+              ),
+
+              SizedBox(height: 10),
+
+              ListTile(
+                leading: Icon(EneftyIcons.chart_success_outline, color: black),
+                title: Text('Statistical Analysis', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500, color: black),),
+                subtitle: Text('Students will delve into statistical concepts essential for data analysis, including probability distributions, hypothesis testing, and regression analysis.', 
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400, color: black),),
+              ),
+
+              SizedBox(height: 10),
+
+              ListTile(
+                leading: Icon(EneftyIcons.chart_success_outline, color: black),
+                title: Text('Machine Learning Basics', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500, color: black),),
+                subtitle: Text('An introduction to machine learning algorithms and techniques, including supervised and unsupervised learning, classification, regression, and clustering.', 
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400, color: black),),
+              ),
+
+              SizedBox(height: 10),
+
+              ListTile(
+                leading: Icon(EneftyIcons.chart_success_outline, color: black),
+                title: Text('Introduction to Python for Data Science', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500, color: black),),
+                subtitle: Text('A hands-on introduction to the Python programming language, focusing on its applications in data science, data manipulation with Pandas, and data visualization with Matplotlib and Seaborn.', 
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400, color: black),),
+              ),
+
+              SizedBox(height: 10),
+
+              ListTile(
+                leading: Icon(EneftyIcons.chart_success_outline, color: black),
+                title: Text('Capstone Project', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500, color: black),),
+                subtitle: Text('Students will apply their knowledge and skills acquired throughout the course to complete a real-world data science project. This project will involve data acquisition, preprocessing, analysis, modeling, and presentation of findings.', 
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400, color: black),),
+              ),
+            ],
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class AboutCourse extends StatelessWidget {
+  const AboutCourse({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -214,10 +292,10 @@ class AboutCourse extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
             // About Course
             Container(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     'About Course',
@@ -226,31 +304,26 @@ class AboutCourse extends StatelessWidget {
                       fontWeight: FontWeight.w600,
                       color: black,
                     ),
-                  
-                  
                   ),
-
                   SizedBox(height: 10),
-
-            Text(
-              'In this introductory course to data science, students will embark on a journey to understand the fundamental concepts, techniques, and tools used in the field of data science. The course aims to equip learners with a solid foundation in data science principles, enabling them to extract insights from data and make informed decisions.',
-              textAlign: TextAlign.justify,
-              style: GoogleFonts.poppins(
-                fontSize: 15,
-                fontWeight: FontWeight.w400,
-                color: black,
-              ),
-            ),
+                  Text(
+                    'In this introductory course to data science, students will embark on a journey to understand the fundamental concepts, techniques, and tools used in the field of data science. The course aims to equip learners with a solid foundation in data science principles, enabling them to extract insights from data and make informed decisions.',
+                    textAlign: TextAlign.justify,
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: black,
+                    ),
+                  ),
                 ],
               ),
-            
             ),
-
             SizedBox(height: 20),
 
-
-             Container(
+            // Prerequisites
+            Container(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     'Prerequisites',
@@ -259,32 +332,78 @@ class AboutCourse extends StatelessWidget {
                       fontWeight: FontWeight.w600,
                       color: black,
                     ),
-                  
-                  
                   ),
-
                   SizedBox(height: 10),
-
-                 ListView(
-                  children: [
-                    ListTile(
+                  ListView(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      ListTile(
                         leading: Icon(EneftyIcons.check_outline),
-                        title: Text('Basic understanding of mathematics and statistics'),
+                        title: Text(
+                            'Basic understanding of mathematics and statistics'),
                       ),
                       ListTile(
                         leading: Icon(EneftyIcons.chrome_outline),
-                        title: Text('Familiarity with programming concepts (preferably Python)'),
-            ),
-                  ],
-                 )
+                        title: Text(
+                            'Familiarity with programming concepts (preferably Python)'),
+                      ),
+                    ],
+                  )
                 ],
               ),
-            
+            ),
+
+            SizedBox(height: 20),
+
+            // Who can take this course
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Who can take this course',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: black,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'This course is ideal for beginners with an interest in data science and analytics. It is suitable for students, professionals, and anyone looking to embark on a career in data science or related fields.',
+                    textAlign: TextAlign.justify,
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: black,
+                    ),
+                  ),
+                ],
+              ),
             ),
             
-          ],
-        ),
-      ),
-    );
+            SizedBox(height: 20),
+
+            //enroll button
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: darkblue,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text('Enroll', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w600, color: white),),
+                            ),
+              ],
+            ),
+          ]
+        )
+      )
+        );
+      
   }
 }
